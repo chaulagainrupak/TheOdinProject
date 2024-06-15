853d8148058943fd2c6c6af5f06466e5efaee476
chaulagainrupak <rupak.chaulagainO19@gmail.com>
2024-06-15 20:45:28 +0545

    added some library features for displaying books and handling of the books. Will finish it tomorrow i guess.

--- Library/script.js
+++ Library/script.js
@@ -1,3 +1,5 @@
+myLibrary = [];
+
 function books(name, author, pages, read){
     this.name = name;
     this.author = author;
@@ -14,10 +16,58 @@ function books(name, author, pages, read){
         console.log(name + ' by '+ author +', ' + pages + ' pages' + ', ' + read);
         return name + ' by '+ author +', ' + pages + ' pages' + ', ' + read;
     }
+
+    this.write = function () {
+        myLibrary.push(
+            {
+                name: name,
+                author: author,
+                pages: pages,
+                read: read
+            }
+        )
+    }
 }
 
 
 let book = new books('book', 'writer', 69, true)
 
-book.info();
 
+window.onload  = function  (){
+    book.info();
+    book.write();
+    console.log(myLibrary);
+
+
+    let div = document.querySelector('.books');
+    let newBook = document.createElement('div');
+    newBook.textContent = myLibrary[0].name;
+    div.appendChild(newBook);
+
+}
+
+
+
+let shown = false;
+let addBook= () => {
+    let form = document.getElementById('bookForm');
+    if(!shown){
+        form.style.display = 'block';
+        shown = true;
+    }else{
+        form.style.display = 'none';
+        shown = false;
+    }
+}
+
+
+document.addEventListener('submit', function (event){
+    event.preventDefault();
+
+    let book = document.getElementById('book').value;
+
+    myLibrary.push(book);
+    console.log(myLibrary);
+
+    document.getElementById('book').value = '';
+});
