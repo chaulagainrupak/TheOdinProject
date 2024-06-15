7a6628dbcad554261d8be4637a1777ae4a581c06
chaulagainrupak <rupak.chaulagainO19@gmail.com>
2024-06-15 19:19:10 +0545

    Object testing

---
+++ Library/script.js
@@ -0,0 +1,24 @@
+function books(name, author, pages, read){
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function () {
+
+        if(read == true){
+            read = 'read'
+        }else {
+            read = 'not read yet!'
+        }
+        console.log(name + ' by '+ author +', ' + pages + ' pages' + ', ' + read);
+        return name + ' by '+ author +', ' + pages + ' pages' + ', ' + read;
+    }
+}
+
+
+const book = new books('book', 'writer', 69, true)
+
+book.info();
+
+console.log(Object.getPrototypeOf(book) === books.prototype);
